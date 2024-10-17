a409087147f46a0d35cf504d9f7797b1691ad5c6
Ishan Bansal <ishan.13b@gmail.com>
2024-10-17 15:44:12 -0500

    Initial commit

---
+++ 1a_OpenAI_Chatbot/app.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import openai
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+## Langsmith Tracking
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_PROJECT"]="Q&A Chatbot With OPENAI"
+
+## Prompt Template
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant. Please respond to the user queries"),
+        ("user","question:{question}")
+    ]
+)
+
+def generate_response(question,engine,temperature,max_tokens):
+    openai.api_key=os.environ["OPENAI_API_KEY"]
+    llm=ChatOpenAI(model=engine)
+    output_parser = StrOutputParser()
+    chain = prompt|llm|output_parser
+    answer = chain.invoke({'question':question})
+    return answer
+
+## Title of the app
+st.title("Enchanced Q&A Chatbot With OPENAI")
+
+## Sidebar for settings
+#st.sidebar.title("Settings")
+#api_key = st.sidebar.text_input("Enter your Open AI Key:",type="password")
+
+## Drop down to select various Open AI Models
+engine = st.sidebar.selectbox("Select an Open AI Model",["gpt-4o","gpt-4-turbo","gpt-4"])
+
+## Adjust response parameter
+temperature = st.sidebar.slider("Temperature",min_value=0.0,max_value=1.0,value=0.7)
+max_tokens = st.sidebar.slider("Max Tokens",min_value=50,max_value=300,value=150)
+
+## Main Interface for user input
+st.write("Go ahead and ask any question")
+user_input = st.text_input("You:")
+
+if user_input:
+    response = generate_response(user_input,engine,temperature,max_tokens)
+    st.write(response)
+elif user_input:
+    st.warning("Please enter the Open AI API Key in the sider bar")
+else:
+    st.write("Please provide the user input")
